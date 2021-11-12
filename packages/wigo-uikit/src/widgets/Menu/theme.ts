9bd8592d33908b3ae28eac4824c817959e5d7dff
a.sabzalian <wearewigo@protonmail.com>
2021-11-13 01:45:31 +0330

    fix: Set primary menu color

--- packages/wigo-uikit/src/widgets/Menu/theme.ts
+++ packages/wigo-uikit/src/widgets/Menu/theme.ts
@@ -5,9 +5,9 @@ export interface NavThemeType {
 }
 
 export const light: NavThemeType = {
-  background: lightColors.backgroundAlt,
+  background: lightColors.primaryMenu,
 };
 
 export const dark: NavThemeType = {
-  background: darkColors.backgroundAlt,
+  background: darkColors.primaryMenu,
 };
