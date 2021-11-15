d5506dccaf138b6166fe75e8f627157d710563dc
a.sabzalian <wearewigo@protonmail.com>
2021-11-16 00:59:55 +0330

    fix: Fix skelton color

--- packages/wigo-uikit/src/theme/colors.ts
+++ packages/wigo-uikit/src/theme/colors.ts
@@ -41,7 +41,7 @@ export const lightColors: Colors = {
   textDisabled: "#BDC2C4",
   textSubtle: "#7A6EAA",
   disabled: "#E9EAEB",
-  skelton: "#C7C7C7",
+  skelton: "#0e2046",
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #E5FDFF 0%, #F3EFFF 100%)",
     inverseBubblegum: "linear-gradient(139.73deg, #F3EFFF 0%, #E5FDFF 100%)",
@@ -77,7 +77,7 @@ export const darkColors: Colors = {
   textDisabled: "#666171",
   textSubtle: "#C7C7C7",
   disabled: "#524B63",
-  skelton: "#C7C7C7",
+  skelton: "#0e2046",
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
     inverseBubblegum: "linear-gradient(139.73deg, #3D2A54 0%, #313D5C 100%)",
