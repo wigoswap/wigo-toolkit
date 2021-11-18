6c8de9ac7ed691792189389da93c935b9ae36aa7
a.sabzalian <wearewigo@protonmail.com>
2021-11-18 21:15:05 +0330

    fix: Fix spinner

--- packages/wigo-uikit/src/components/Spinner/Spinner.tsx
+++ packages/wigo-uikit/src/components/Spinner/Spinner.tsx
@@ -5,23 +5,11 @@ import { SpinnerProps } from "./types";
 
 const rotate = keyframes`
   from {
-    transform: rotate(0deg);
+    transform: rotate(90deg);
   }
   to {
-    transform: rotate(360deg);
+    transform: rotate(0deg);
   }
-`;
-
-const float = keyframes`
-	0% {
-		transform: translatey(0px);
-	}
-	50% {
-		transform: translatey(10px);
-	}
-	100% {
-		transform: translatey(0px);
-	}
 `;
 
 const Container = styled.div`
@@ -32,7 +20,8 @@ const RotatingWigoIcon = styled(Logo)`
   position: absolute;
   top: 0;
   left: 0;
-  animation: ${rotate} 2s linear infinite;
+  animation: ${rotate} 2s ease-out infinite;
+  animation-delay: 0.5s;
   transform: translate3d(0, 0, 0);
 `;
 
