aa5a866120e1143e711fe0b24fa9128787b57c99
a.sabzalian <wearewigo@protonmail.com>
2021-11-20 20:12:49 +0330

    fix: Fix mobile menu height

--- packages/wigo-uikit/src/components/BottomNavItem/styles.tsx
+++ packages/wigo-uikit/src/components/BottomNavItem/styles.tsx
@@ -1,12 +1,13 @@
 import styled from "styled-components";
 import { Text } from "../Text";
+import { MOBILE_MENU_HEIGHT } from "../../widgets/Menu/config";
 
 export const StyledBottomNavItem = styled.button`
   display: block;
   border: 0;
   background: transparent;
   cursor: pointer;
-  height: 44px;
+  height: ${MOBILE_MENU_HEIGHT}px;
   padding: 4px 12px;
   &:hover {
     border-radius: 16px;
