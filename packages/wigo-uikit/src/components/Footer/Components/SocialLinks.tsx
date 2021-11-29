ccc06f76027fd76d67b57bd3891e568b5a0bdb8c
a.sabzalian <wearewigo@protonmail.com>
2021-11-29 14:24:16 +0330

    fix: Fix social media icons

--- packages/wigo-uikit/src/components/Footer/Components/SocialLinks.tsx
+++ packages/wigo-uikit/src/components/Footer/Components/SocialLinks.tsx
@@ -17,17 +17,17 @@ const SocialLinks: React.FC<FlexProps> = ({ ...props }) => (
         style: { cursor: "pointer" },
       };
       const mr = index < socials.length - 1 ? "24px" : 0;
-      if (social.items) {
-        return (
-          <Dropdown key={social.label} position="top" target={<IconComponent {...iconProps} mr={mr} />}>
-            {social.items.map((item) => (
-              <Link external key={item.label} href={item.href} aria-label={item.label} color="textSubtle">
-                {item.label}
-              </Link>
-            ))}
-          </Dropdown>
-        );
-      }
+      // if (social.items) {
+      //   return (
+      //     <Dropdown key={social.label} position="top" target={<IconComponent {...iconProps} mr={mr} />}>
+      //       {social.items.map((item) => (
+      //         <Link external key={item.label} href={item.href} aria-label={item.label} color="textSubtle">
+      //           {item.label}
+      //         </Link>
+      //       ))}
+      //     </Dropdown>
+      //   );
+      // }
       return (
         <Link external key={social.label} href={social.href} aria-label={social.label} mr={mr}>
           <IconComponent {...iconProps} />
--- packages/wigo-uikit/src/components/Footer/config.tsx
+++ packages/wigo-uikit/src/components/Footer/config.tsx
@@ -80,79 +80,77 @@ export const socials = [
     label: "Twitter",
     icon: "Twitter",
     href: "https://twitter.com/wigoswap",
+    items: [],
   },
+  // {
+  //   label: "Telegram",
+  //   icon: "Telegram",
+  //   items: [
+  //     {
+  //       label: "English",
+  //       href: "https://t.me/wigoswap",
+  //     },
+  //     {
+  //       label: "Bahasa Indonesia",
+  //       href: "https://t.me/WigoSwapIndonesia",
+  //     },
+  //     {
+  //       label: "中文",
+  //       href: "https://t.me/WigoSwap_CN",
+  //     },
+  //     {
+  //       label: "Tiếng Việt",
+  //       href: "https://t.me/WigoSwapVN",
+  //     },
+  //     {
+  //       label: "Italiano",
+  //       href: "https://t.me/wigoswap_ita",
+  //     },
+  //     {
+  //       label: "русский",
+  //       href: "https://t.me/wigoswap_ru",
+  //     },
+  //     {
+  //       label: "Türkiye",
+  //       href: "https://t.me/wigoswapturkiye",
+  //     },
+  //     {
+  //       label: "Português",
+  //       href: "https://t.me/WigoSwapPortuguese",
+  //     },
+  //     {
+  //       label: "Español",
+  //       href: "https://t.me/WigoswapEs",
+  //     },
+  //     {
+  //       label: "日本語",
+  //       href: "https://t.me/wigoswapjp",
+  //     },
+  //     {
+  //       label: "Français",
+  //       href: "https://t.me/wigoswapfr",
+  //     },
+  //     {
+  //       label: "Announcements",
+  //       href: "https://t.me/WigoSwapAnn",
+  //     },
+  //     {
+  //       label: "Whale Alert",
+  //       href: "https://t.me/WigoSwapWhales",
+  //     },
+  //   ],
+  // },
   {
     label: "Telegram",
     icon: "Telegram",
-    items: [
-      {
-        label: "English",
-        href: "https://t.me/wigoswap",
-      },
-      {
-        label: "Bahasa Indonesia",
-        href: "https://t.me/WigoSwapIndonesia",
-      },
-      {
-        label: "中文",
-        href: "https://t.me/WigoSwap_CN",
-      },
-      {
-        label: "Tiếng Việt",
-        href: "https://t.me/WigoSwapVN",
-      },
-      {
-        label: "Italiano",
-        href: "https://t.me/wigoswap_ita",
-      },
-      {
-        label: "русский",
-        href: "https://t.me/wigoswap_ru",
-      },
-      {
-        label: "Türkiye",
-        href: "https://t.me/wigoswapturkiye",
-      },
-      {
-        label: "Português",
-        href: "https://t.me/WigoSwapPortuguese",
-      },
-      {
-        label: "Español",
-        href: "https://t.me/WigoswapEs",
-      },
-      {
-        label: "日本語",
-        href: "https://t.me/wigoswapjp",
-      },
-      {
-        label: "Français",
-        href: "https://t.me/wigoswapfr",
-      },
-      {
-        label: "Announcements",
-        href: "https://t.me/WigoSwapAnn",
-      },
-      {
-        label: "Whale Alert",
-        href: "https://t.me/WigoSwapWhales",
-      },
-    ],
-  },
-  {
-    label: "Reddit",
-    icon: "Reddit",
-    href: "https://reddit.com/r/wigoswap",
-  },
-  {
-    label: "Instagram",
-    icon: "Instagram",
-    href: "https://instagram.com/wigoswap_official",
+    href: "https://t.me/wigoswap",
+    items: [],
   },
   {
     label: "Github",
     icon: "Github",
     href: "https://github.com/wigoswap/",
+    items: [],
   },
 ];
 
