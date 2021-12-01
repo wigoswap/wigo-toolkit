be191cf31f927e6b4491337c5eaef16e46e452a5
a.sabzalian <wearewigo@protonmail.com>
2021-12-01 20:30:11 +0330

    fix: Fix toggle

--- packages/wigo-uikit/src/components/Toggle/StyledToggle.tsx
+++ packages/wigo-uikit/src/components/Toggle/StyledToggle.tsx
@@ -12,13 +12,13 @@ const scaleKeyValues = {
     toggleWidth: "36px",
   },
   md: {
-    handleHeight: "26px",
-    handleWidth: "26px",
+    handleHeight: "22px",
+    handleWidth: "22px",
     handleLeft: "3px",
     handleTop: "3px",
-    checkedLeft: "calc(100% - 30px)",
-    toggleHeight: "32px",
-    toggleWidth: "56px",
+    checkedLeft: "calc(100% - 25px)",
+    toggleHeight: "28px",
+    toggleWidth: "53px",
   },
   lg: {
     handleHeight: "32px",
--- packages/wigo-uikit/src/components/Toggle/theme.ts
+++ packages/wigo-uikit/src/components/Toggle/theme.ts
@@ -1,13 +1,13 @@
-import { darkColors, lightColors } from "../../theme/colors";
+import { baseColors } from "../../theme/colors";
 
 export type ToggleTheme = {
   handleBackground: string;
 };
 
 export const light: ToggleTheme = {
-  handleBackground: lightColors.backgroundAlt,
+  handleBackground: baseColors.wigoWhite,
 };
 
 export const dark: ToggleTheme = {
-  handleBackground: darkColors.backgroundAlt,
+  handleBackground: baseColors.wigoWhite,
 };
