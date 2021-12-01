b4b9a39f84e45267844a83d424c952e4f356a077
a.sabzalian <wearewigo@protonmail.com>
2021-12-01 16:14:12 +0330

    fix: Fix Modal

--- packages/wigo-uikit/src/widgets/Modal/Modal.tsx
+++ packages/wigo-uikit/src/widgets/Modal/Modal.tsx
@@ -23,7 +23,7 @@ const Modal: React.FC<ModalProps> = ({
       <ModalHeader background={getThemeValue(`colors.${headerBackground}`, headerBackground)(theme)}>
         <ModalTitle>
           {onBack && <ModalBackButton onBack={onBack} />}
-          <Heading color="titleColor">{title}</Heading>
+          <Heading color={titleColor}>{title}</Heading>
         </ModalTitle>
         {!hideCloseButton && <ModalCloseButton onDismiss={onDismiss} />}
       </ModalHeader>
