2c79fddc330dc3a7d5e87404f3b98f23af65c599
a.sabzalian <wearewigo@protonmail.com>
2021-12-02 20:02:18 +0330

    fix: Fix button menu colors

--- packages/wigo-uikit/src/components/ButtonMenu/ButtonMenuItem.tsx
+++ packages/wigo-uikit/src/components/ButtonMenu/ButtonMenuItem.tsx
@@ -10,9 +10,10 @@ interface InactiveButtonProps extends BaseButtonProps {
 
 const InactiveButton: PolymorphicComponent<InactiveButtonProps, "button"> = styled(Button)<InactiveButtonProps>`
   background-color: transparent;
-  color: ${({ theme, variant }) => (variant === variants.PRIMARY ? theme.colors.primary : theme.colors.textSubtle)};
+  color: ${({ theme, variant }) =>
+    variant === variants.PRIMARY ? theme.colors.primary : theme.colors.textSubtle} !important;
   &:hover:not(:disabled):not(:active) {
-    background-color: transparent;
+    background-color: transparent !important;
   }
 `;
 
