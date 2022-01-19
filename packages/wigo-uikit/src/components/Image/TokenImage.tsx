4e652c6d4a213c9b0e5d71b4862f6c3b5ab80b89
WigoSwap <wearewigo@protonmail.com>
2022-01-19 13:24:58 +0100

    fix: Fix some colors

--- packages/wigo-uikit/src/components/Image/TokenImage.tsx
+++ packages/wigo-uikit/src/components/Image/TokenImage.tsx
@@ -5,7 +5,7 @@ const TokenImage = styled(Image)`
   &:before {
     border-radius: 50%;
     content: "";
-    box-shadow: -1px -1px 10px -1px rgb(21 32 41);
+    box-shadow: -1px -1px 10px -1px rgb(22 28 31);
     height: 100%;
     left: 0;
     position: absolute;
