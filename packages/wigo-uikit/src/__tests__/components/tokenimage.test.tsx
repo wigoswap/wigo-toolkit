7cd81a91efa5ce4305de0cdf29b32e15568f24a1
WigoSwap <wearewigo@protonmail.com>
2022-01-24 14:32:04 +0100

    fix: Fix Wigo token address

--- packages/wigo-uikit/src/__tests__/components/tokenimage.test.tsx
+++ packages/wigo-uikit/src/__tests__/components/tokenimage.test.tsx
@@ -6,7 +6,7 @@ it("renders correctly", () => {
   setupMockIntersectionObserver();
   const { asFragment } = renderWithTheme(
     <TokenImage
-      src="https://wigoswap.io/images/tokens/0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82.svg"
+      src="https://wigoswap.io/images/tokens/0xE992bEAb6659BFF447893641A378FbbF031C5bD6.svg"
       height={48}
       width={48}
     />
--- packages/wigo-uikit/src/components/Image/tokens.ts
+++ packages/wigo-uikit/src/components/Image/tokens.ts
@@ -5,7 +5,7 @@ const tokens = {
   wigo: {
     symbol: "WIGO",
     address: {
-      56: "0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82",
+      56: "0xE992bEAb6659BFF447893641A378FbbF031C5bD6",
       97: "0xa35062141Fa33BCA92Ce69FeD37D0E8908868AAe",
     },
     decimals: 18,
