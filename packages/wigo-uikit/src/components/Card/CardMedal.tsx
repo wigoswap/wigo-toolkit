467adfc21ad2e6edfaee324e6076a0dd479ef31e
WigoSwap <wearewigo@protonmail.com>
2022-07-25 13:25:05 +0200

    fix: Fix CardMedal

--- packages/wigo-uikit/src/components/Card/CardMedal.tsx
+++ packages/wigo-uikit/src/components/Card/CardMedal.tsx
@@ -17,14 +17,14 @@ const StyledCardMedal = styled.div<Partial<StyledCardMedalProps>>`
   left: ${({ medalPosition }) => (medalPosition === "left" ? "20px" : "auto")};
   top: 0;
   text-align: center;
-  width: 100px;
+  width: 94px;
 
   & > div {
     overflow: hidden;
     text-overflow: ellipsis;
     white-space: nowrap;
-    width: 100px;
-    bottom: 25px;
+    width: 94px;
+    bottom: 23px;
     position: absolute;
     color: ${({ theme }) => theme.card.background};
     font-weight: bold;
